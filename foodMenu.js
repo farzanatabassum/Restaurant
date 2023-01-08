87c06b94b8d509c44237944bb8a38c7b6d0f16d7
sakibraihan47 <83722134+sakibraihan47@users.noreply.github.com>
2023-01-08 17:37:53 +0600

    feat: #9 Modified Modals and CSS

--- foodMenu.js
+++ foodMenu.js
@@ -8,6 +8,8 @@ function createModal() {
     let modalContent = document.createElement("div")
     modalContent.className="modal-content"
 
+    let heading = document.createElement("div")
+heading.className="modal-heading"
     let close = document.createElement("span")
     close.className="close"
     close.innerText ="X"
@@ -16,8 +18,24 @@ function createModal() {
     title.className="titleModal"
 
     let body = document.getElementsByClassName("body")[0]
+let content = document.createElement("p")
+content.className = "detailsModal"
 
-    modalContent.appendChild(title)
+let price = document.createElement("h1")
+    // let content = document.getElementsByClassName("food-details")[0]
+price.className = "priceModal"
+
+let image = document.createElement("img")
+image.className = "imageModal"
+// content.innerText = nodeElement.target.getElementsByClassName("food-details")[0]
+    // modalContent.appendChild(content)
+heading.appendChild(title)
+heading.appendChild(price)
+
+    modalContent.appendChild(heading)
+    modalContent.appendChild(image)
+
+    modalContent.appendChild(content)
     modalContent.appendChild(close)
     modalBackground.appendChild(modalContent)
     body.appendChild(modalBackground)
@@ -38,13 +56,27 @@ function handleModal(nodeElement){
 console.log("🚀 ~ file: foodMenu.js:38 ~ handleModal ~ nodeElement", nodeElement.target)
 
     let modalTitle = document.getElementsByClassName("titleModal")[0]
+    let content = document.getElementsByClassName("detailsModal")[0]
+    let price = document.getElementsByClassName("priceModal")[0]
+    let image = document.getElementsByClassName("imageModal")[0]
+    console.log("🚀 ~ file: foodMenu.js:48 ~ handleModal ~ content", content)
     let modal = document.getElementsByClassName("modal")[0]
     let close = document.getElementsByClassName("close")[0]
+let cardDetails = nodeElement.target.getElementsByClassName("food-details")[0]
+  console.log("🚀 ~ file: foodMenu.js:52 ~ handleModal ~ cardDetails", cardDetails)
   let cardTitle = nodeElement.target.getElementsByClassName("food-title")[0]
     console.log("🚀 ~ file: foodMenu.js:44 ~ handleModal ~ cardTitle", cardTitle)
+
+    let priceTag = nodeElement.target.getElementsByClassName("food-price")[0]
+
+    let imageView = nodeElement.target.getElementsByTagName("img")[0]
+    console.log("🚀 ~ file: foodMenu.js:68 ~ handleModal ~ imageView", imageView)
     
         modal.style.display = "block"
         modalTitle.innerText = cardTitle.innerText
+        content.innerText = cardDetails.innerText
+        price.innerText = priceTag.innerText
+        image.src = imageView.src
         console.log("🚀 ~ file: foodMenu.js:38 ~ title.addEventListener ~ modal", modal)
     
     close.addEventListener("click",()=>{
