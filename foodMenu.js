b91adb1cdebe1e9b1e242d12600e4d52ceb3cc23
Farzana Tabassum <82896277+farzanatabassum@users.noreply.github.com>
2023-01-08 14:22:29 +0600

    feat: #8 menu data is displayed

--- foodMenu.js
+++ foodMenu.js
@@ -1,1 +1,15 @@
-console.log('Hello')
+const filterItems=(foodCategory)=>{
+    fetch('./menu.json')
+    .then((response) => response.json())
+    .then((data) => 
+    {
+        let modifydata=data.filter((foodItem)=>{
+        
+        if(foodItem.category===foodCategory){
+            return foodItem
+        }
+       
+    })
+    return modifydata}
+    )
+}
