c827fcd1c4a30a5df0f03ce0aa14fab5d3f80205
sakibraihan47 <83722134+sakibraihan47@users.noreply.github.com>
2023-01-08 13:59:18 +0600

    feat: #9 created modal in foodMenu.js

--- foodMenu.js
+++ foodMenu.js
@@ -1,1 +1,54 @@
 console.log('Hello')
+
+function createModal() {
+
+    let modalBackground = document.createElement("div")
+    modalBackground.className="modal"
+
+    let modalContent = document.createElement("div")
+    modalContent.className="modal-content"
+
+    let close = document.createElement("span")
+    close.className="close"
+    close.innerText ="X"
+
+    let body = document.getElementsByClassName("body")[0]
+
+    modalContent.appendChild(close)
+    modalBackground.appendChild(modalContent)
+    body.appendChild(modalBackground)
+
+}
+
+function init(){
+
+createModal()
+handleModal()
+}
+
+init()
+
+function handleModal(){
+
+    let title = document.getElementsByClassName("title")[0]
+    let modal = document.getElementsByClassName("modal")[0]
+    let close = document.getElementsByClassName("close")[0]
+    title.addEventListener("click",()=>{
+        console.log('click')
+        modal.style.display = "block"
+        console.log("🚀 ~ file: foodMenu.js:38 ~ title.addEventListener ~ modal", modal)
+    })
+
+    close.addEventListener("click",()=>{
+      
+        modal.style.display = "none"
+      
+    })
+
+   modal.addEventListener("click",()=>{
+modal.style.display = "none"
+   })
+
+
+}
+
