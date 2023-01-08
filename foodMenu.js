21fe4d04d5b925d907a1dfe0be6b5fe00be39466
sakibraihan47 <83722134+sakibraihan47@users.noreply.github.com>
2023-01-08 14:28:57 +0600

    feat: #9 Modal Created for each card

--- foodMenu.js
+++ foodMenu.js
@@ -12,33 +12,41 @@ function createModal() {
     close.className="close"
     close.innerText ="X"
 
+    let title = document.createElement("h1")
+    title.className="titleModal"
+
     let body = document.getElementsByClassName("body")[0]
 
+    modalContent.appendChild(title)
     modalContent.appendChild(close)
     modalBackground.appendChild(modalContent)
     body.appendChild(modalBackground)
+
 
 }
 
 function init(){
 
 createModal()
-handleModal()
+
+test()
 }
 
 init()
 
-function handleModal(){
+function handleModal(nodeElement){
+console.log("🚀 ~ file: foodMenu.js:38 ~ handleModal ~ nodeElement", nodeElement.target)
 
-    let title = document.getElementsByClassName("title")[0]
+    let modalTitle = document.getElementsByClassName("titleModal")[0]
     let modal = document.getElementsByClassName("modal")[0]
     let close = document.getElementsByClassName("close")[0]
-    title.addEventListener("click",()=>{
-        console.log('click')
+  let cardTitle = nodeElement.target.getElementsByClassName("food-title")[0]
+    console.log("🚀 ~ file: foodMenu.js:44 ~ handleModal ~ cardTitle", cardTitle)
+    
         modal.style.display = "block"
+        modalTitle.innerText = cardTitle.innerText
         console.log("🚀 ~ file: foodMenu.js:38 ~ title.addEventListener ~ modal", modal)
-    })
-
+    
     close.addEventListener("click",()=>{
       
         modal.style.display = "none"
@@ -50,5 +58,25 @@ modal.style.display = "none"
    })
 
 
+   
 }
 
+function test() {
+
+
+let food = document.getElementsByClassName("food-card")
+console.log("🚀 ~ file: foodMenu.js:60 ~ test ~ food", food)
+
+for(let i =0 ; i<food.length; i++){
+
+    food[i].addEventListener('click', handleModal)
+}
+
+
+    
+
+
+
+
+    
+}
